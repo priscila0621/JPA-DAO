3d7d890d49f11cd543d0d5e2efd376c2cc3e9e20
priscila0621 <pjulieths@uamv.edu.ni>
2025-10-22 17:39:15 -0600

    Se crea paquete util, y se crea y configura clase JPAConexion

---
+++ src/main/java/util/JPAConexion.java
@@ -0,0 +1,14 @@
+package util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAConexion {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName:"jpa_db");
+    
+    public static EntityManager getEntityManager() { return emf.createEntityManager();}
+
+    public static void close(){emf.close();}
+    }
+}
