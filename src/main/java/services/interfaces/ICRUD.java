cacf35cd0550d6685b29d300612a13877e9a906f
priscila0621 <pjulieths@uamv.edu.ni>
2025-10-22 17:57:47 -0600

    Se configura ICRUD

--- src/main/java/services/interfaces/ICRUD.java
+++ src/main/java/services/interfaces/ICRUD.java
@@ -1,4 +1,17 @@
 package services.interfaces;
 
-public class ICRUD {
+import java.util.List;
+
+public interface ICRUD {
+
+    <T> List<T> getAll(String nameQuery, Class<T> clazz);
+
+    <T> void insert(T entity);
+
+    <T> void update(T entity);
+
+    <T> void delete(T entity);
+
+    <T> T findById(Integer id, Class<T> clazz);
 }
+
